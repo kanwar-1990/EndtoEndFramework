c31e7d77d87153afe3fff84b1bb3b164ed6031ee
kanwar-1990 <knwrpl.singh@gmail.com>
2022-03-22 19:36:18 +0530

    modified openCart page

--- src/main/java/com/qa/opencart/pages/OpenCartPage.java
+++ src/main/java/com/qa/opencart/pages/OpenCartPage.java
@@ -1,13 +1,14 @@
 package com.qa.opencart.pages;
 
+import com.sun.java_cup.internal.runtime.Symbol;
+
 public class OpenCartPage {
-	
+
 	String cartNu;
-	
-	
-	
-	public void orderNu()
-	{
-		System.out.println("Add the order number");
+
+	public void orderNu(int orderNu) {
+		System.out.println("order nu is ::" + orderNu);
+		System.out.println("cartNu is " + cartNu);
 	}
+
 }
